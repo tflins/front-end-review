cdfed5960828427dd4bc2bf0850d3d41eaf4f96e
tflin <tflins@163.com>
2020-02-21 23:15:48 +0800

    增加 call 相关知识

--- 手写代码/call.js
+++ 手写代码/call.js
@@ -8,6 +8,13 @@ Function.prototype.call2 = function(content = window, ...args) {
   return result
 }
 
+/**
+ * 相关知识
+ * 1. call 与 apply 类似，可以改变函数的执行环境，给函数指定一个 this 指向
+ * 2. call 和 apply 第一个参数都是指定的 this , call 其余参数为传入的参数，apply 第二个参数为一个参数数组
+ * 3. 实现原理与 apply 类似
+ */
+
 // 测试
 function test(name, age) {
   console.log(`my name is ${name}, i am ${age} years old !`)
